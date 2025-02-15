45b54212a3dd2b554cea81abdcb0d8b0bd9a9aac
kzy47 <151009725+kzy47@users.noreply.github.com>
2025-02-15 22:10:18 +0700

    Update ClientInfoPlugin.cs

--- ClientInfoPlugin.cs
+++ ClientInfoPlugin.cs
@@ -2,67 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
+using System.Threading;
 
 namespace DNWS
 {
-  class ClientInfoPlugin : IPlugin
-  {
-    protected static Dictionary<String, int> statDictionary = null;
-    public ClientInfoPlugin()
+    class ClientInfoPlugin : IPlugin
     {
-      if (statDictionary == null)
-      {
-        statDictionary = new Dictionary<String, int>();
+        protected static Dictionary<String, int> statDictionary = new Dictionary<String, int>();
+        private static readonly object lockObject = new object();
 
-      }
+        public void PreProcessing(HTTPRequest request)
+        {
+            Thread thread = new Thread(() => ProcessRequest(request));
+            thread.Start();
+        }
+
+        private void ProcessRequest(HTTPRequest request)
+        {
+            lock (lockObject)
+            {
+                if (statDictionary.ContainsKey(request.Url))
+                {
+                    statDictionary[request.Url]++;
+                }
+                else
+                {
+                    statDictionary[request.Url] = 1;
+                }
+            }
+        }
+
+        public HTTPResponse GetResponse(HTTPRequest request)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            IPEndPoint endpoint;
+            try
+            {
+                endpoint = IPEndPoint.Parse(request.getPropertyByKey("remoteendpoint"));
+            }
+            catch
+            {
+                endpoint = new IPEndPoint(IPAddress.Any, 0);
+            }
+
+            sb.Append("<html><head> <style> body {font-family: courier new; font-size: 14px;} h1 {font-size:30px; color: #4CAF50;} div {line-height: 1.5; margin: 20px;} </style></head>");
+            sb.Append("<body><h1>Client Info:</h1>");
+            sb.AppendFormat("<div><b>Client IP:</b> {0}<br/>", endpoint.Address);
+            sb.AppendFormat("<b>Client Port:</b> {0}<br/>", endpoint.Port);
+            sb.AppendFormat("<b>Browser Information:</b> {0}<br/>", request.getPropertyByKey("user-agent")?.Trim() ?? "Unknown");
+            sb.AppendFormat("<b>Accept Language:</b> {0}<br/>", request.getPropertyByKey("accept-language")?.Trim() ?? "Unknown");
+            sb.AppendFormat("<b>Accept Encoding:</b> {0}<br/></div>", request.getPropertyByKey("accept-encoding")?.Trim() ?? "Unknown");
+            sb.Append("</body></html>");
+
+            HTTPResponse response = new HTTPResponse(200);
+            response.body = Encoding.UTF8.GetBytes(sb.ToString());
+            return response;
+        }
+
+        public HTTPResponse PostProcessing(HTTPResponse response)
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    public void PreProcessing(HTTPRequest request)
-    {
-      if (statDictionary.ContainsKey(request.Url))
-      {
-        statDictionary[request.Url] = (int)statDictionary[request.Url] + 1;
-      }
-      else
-      {
-        statDictionary[request.Url] = 1;
-      }
-    }
-
-    public HTTPResponse GetResponse(HTTPRequest request)
-    {
-      HTTPResponse response = null;
-      StringBuilder sb = new StringBuilder();
-
-      IPEndPoint endpoint = IPEndPoint.Parse(request.getPropertyByKey("remoteendpoint"));
-      // sb.Append("<html><body><pre>");
-      // sb.AppendFormat("Client IP: {0}<br/>\n", endpoint.Address);
-      // sb.AppendFormat("Client Port: {0}<br/>\n", endpoint.Port);
-      // sb.AppendFormat("Browser Information: {0}<br/>\n", request.getPropertyByKey("user-agent").Trim());
-      // sb.AppendFormat("Accept Language: {0}<br/>\n", request.getPropertyByKey("accept-language").Trim());
-      // sb.AppendFormat("Accept Encoding: {0}<br/>\n", request.getPropertyByKey("accept-encoding").Trim());
-
-      // sb.Append("</pre></body></html>");
-
-      sb.Append("<html><head> <style> body {font-family: courier new: 10px;} h1 {font-size:30px; color: #4CAF50;} div {line-height: 1.5; margin: 20px;} </style></head></html>");
-      sb.Append("<html><h1>Client Info:</h1>");
-      sb.AppendFormat("<div><b>Client IP:</b> " + endpoint.Address + "<br/>" );
-      sb.AppendFormat("<b>Client Port:</b> " + endpoint.Port + "<br/>");
-      sb.AppendFormat("<b>Browser Information:</b> " + request.getPropertyByKey("user-agent").Trim() + "<br/>");
-      sb.AppendFormat("<b>Accept Language:</b> " + request.getPropertyByKey("accept-language").Trim() + "<br/>");
-      sb.AppendFormat("<b>Accept Encoding:</b> " + request.getPropertyByKey("accept-encoding").Trim() + "<br/><div>");
-      
-      sb.Append("</body></html>");
-
-      response = new HTTPResponse(200);
-      response.body = Encoding.UTF8.GetBytes(sb.ToString());
-      return response;
-    }
-
-
-    public HTTPResponse PostProcessing(HTTPResponse response)
-    {
-      throw new NotImplementedException();
-    }
-  }
 }
